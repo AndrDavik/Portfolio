712542f389d315648d91d4f300248075b6c125bf
Andrey <davikandrey@gmail.com>
2022-07-06 20:57:01 +0300

    mod contacts

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
@@ -31,3 +33,116 @@ for (let anchor of anchors) {
         });
     });
 }
+
+
+//Validation and sending emails
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = this.getElementById('form');
+//     form.addEventListener('submit', formSend);
+
+//     async function formSend(e) {
+//         e.preventDefault();
+
+//         let error = formValidate(form);
+//     }
+
+//     function formValidate(form) {
+//         let error = 0;
+//         let formReq = document.querySelectorAll('._req');
+
+//         for (let index = 0; index < formReq.length; index++) {
+//             const input = formReq[index];
+//             formRemoveError(input);
+
+//             if (input.classList.contains('_email')) {
+//                 if (emailTest(input)) {
+//                     formAddError(input);
+//                     error++;
+//                 }
+//             } else {
+//                 if (input.value === '') {
+//                     formAddError(input);
+//                     error++; 
+//                 }
+
+//             }
+//         }
+//     }
+
+//     function formAddError(input) {
+//         input.parentElement.classList.add('_error');
+//         input.classList.add('_error');
+//     }
+//     function formRemoveError(input) {
+//         input.parentElement.classList.Remove('_error');
+//         input.classList.Remove('_error');
+//     }
+
+//     //function test email
+//     function emailTest(input) {
+//         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//     } 
+
+// });
+
+const forms = () =>{ 
+    const form = document.querySelector('form'),
+        inputs = document.querySelectorAll('input');
+ 
+ 
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Мы с вами скоро свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+ 
+    const postData = async(url, data) =>{
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+ 
+        return await res.text();
+    };
+ 
+    //переменная с функцией по очистке input
+    const clearInput = () =>{
+        inputs.forEach(item =>{
+            item.value = '';
+        });
+    };
+ 
+ 
+    
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault(); 
+ 
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        form.appendChild(statusMessage); 
+ 
+ 
+        const formData = new FormData(form);
+ 
+        postData('mailer/smart.php', formData)
+        .then(res =>{
+            console.log(res);
+            statusMessage.textContent= message.success;
+        })
+        .catch ( ()=>{
+            statusMessage.textContent= message.failure;
+        })
+        .finally ( ()=>{
+            clearInput();
+            setTimeout ( ()=>{
+                statusMessage.remove();
+            },5000);
+        });
+ 
+ 
+    });
+    
+};
+ 
+forms();        
